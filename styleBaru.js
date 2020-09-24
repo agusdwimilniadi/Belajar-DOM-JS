da2047287299db0d40f9ab07c191d870ee270cf7
agusdwimilniadi <agusdwimill@gmail.com>
2020-09-24 13:51:19 +0700

    komentari

--- styleBaru.js
+++ styleBaru.js
@@ -1,13 +1,11 @@
-const par3 = document.querySelector('section#a p.p3');
-const par2 = document.querySelector('section#a p.p2');
+// const par3 = document.querySelector('section#a p.p3');
+// const par2 = document.querySelector('section#a p.p2');
 
-function ubahWarna() {
-    par2.style.backgroundColor = 'red';
-}
+// function ubahWarna() {
+//     par2.style.backgroundColor = 'red';
+// }
 
-par2.addEventListener('click', ubahWarna);
-
-
+// par2.addEventListener('click', ubahWarna);
 
 
 
@@ -23,14 +21,19 @@ par2.addEventListener('click', ubahWarna);
 
 
 
-const p4 = document.querySelector('section#b p');
-p4.addEventListener('click', function(){
-    const ul = document.querySelector('section#b ul');
-    const liBaru = document.createElement('li');
 
-    const teksLiBaru = document.createTextNode('ITEM BARU DI LI');
-    liBaru.appendChild(teksLiBaru);
-    ul.appendChild(liBaru);
 
-});
+// const p4 = document.querySelector('section#b p');
+// p4.addEventListener('click', function(){
+//     const ul = document.querySelector('section#b ul');
+//     const liBaru = document.createElement('li');
+
+//     const teksLiBaru = document.createTextNode('ITEM BARU DI LI');
+//     liBaru.appendChild(teksLiBaru);
+//     ul.appendChild(liBaru);
+
+// });
+
+
+
 
